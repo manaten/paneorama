0765e8d94dea512bde255bcf948cb7887a336cd9
manaten <586064+manaten@users.noreply.github.com>
2025-09-19 09:36:37 +0900

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/paneorama/",
+  base: "/",
   root: "./src",
   publicDir: "../public",
   plugins: [react(), svgr(), tailwindcss()],
